95b987c0b287a6192b178e8f77d22f957bcc260b
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-04-15 11:47:09 +0900

    first commit’

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -60,5 +60,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 end
